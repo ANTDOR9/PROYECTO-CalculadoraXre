3db04d2c7140ca707557a92fa8414e714b6e8e8e
Anthony Dorly Huilahuaña Chata <1677406@senati.pe>
2026-01-19 09:55:18 -0500

    LOGIN

---
+++ js/auth.js
@@ -0,0 +1,27 @@
+// =========================
+// LOGIN SIMULADO
+// =========================
+
+// Credenciales falsas
+const USER = "admin";
+const PASS = "1234";
+
+// Elementos
+const loginForm = document.getElementById("loginForm");
+const msg = document.getElementById("loginMsg");
+
+// Login
+loginForm.addEventListener("submit", e => {
+    e.preventDefault();
+
+    const user = document.getElementById("loginUser").value;
+    const pass = document.getElementById("loginPass").value;
+
+    if (user === USER && pass === PASS) {
+        localStorage.setItem("logged", "true");
+        window.location.href = "index.html";
+    } else {
+        msg.textContent = "Usuario o contraseña incorrectos";
+        msg.style.color = "var(--accent-color)";
+    }
+});
